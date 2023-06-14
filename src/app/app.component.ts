db367d0e97a4fe69a3bc430b79f3c83b68a5583d
JUAN FELIPE BARAHONA GONZALEZ <jbarahona@xm.com.co>
2023-06-14 00:56:42 -0500

    logging service was added to app component file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,28 @@
+import { LoggingService } from './logging.service';
 import { Component } from '@angular/core';
-import { Person, IName, nameTemplate } from './person.model';
+import { Person } from './person.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  constructor(private loggingService: LoggingService) {}
+
   title = 'Persons list';
   persons: Person[] = [
     new Person({ first: 'Juan', last: 'Barahona' }),
-    new Person({ first: 'Viviana', last: 'Delgado' })
+    new Person({ first: 'Viviana', last: 'Delgado' }),
   ];
 
   addNewPerson(person: Person) {
     this.persons.push(person);
+    this.loggingService.log(`New persons length -> ${this.persons.length}`);
   }
 
   clearList() {
     this.persons = [];
+    this.loggingService.log(`New persons length -> ${this.persons.length}`);
   }
 }
